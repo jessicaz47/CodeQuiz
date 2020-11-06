dcacfc749e191c480defb8a262a54630bf2422c5
Jessica Hamilton <jesszonta@gmail.com>
2020-11-06 16:33:10 -0500

    first commit

---
+++ quiz.js
@@ -0,0 +1,22 @@
+var myQuestions = [
+    {
+        question: "Which of the following function of String object returns the calling string value converted to upper case?",
+        choices: ["toLocaleUpperCase()", "toUpperCase", "toUpper()", "uppercase()"],
+        correctAnswer: 1
+    },
+    {
+        question: "Which of the following function of String object returns the characters in a string between two indexes into the string?",
+        choices: ["substring()", "split()", "seperate()", "cut()"],
+        correctAnswer: 0
+    },
+    {
+        question: "Which built-in method returns the index within the calling String object of the first occurrence of the specified value?",
+        choices: ["locate()", "indexOf()", "Index()", "string()"],
+        correctAnswer: 1
+    },
+    {
+        question: "Which built-in method reverses the order of the elements of an array?",
+        choices: ["reverse()", "switch()", "reverseOrder()", "changeOrder()"],
+        correctAnswer: 0
+    },
+]
